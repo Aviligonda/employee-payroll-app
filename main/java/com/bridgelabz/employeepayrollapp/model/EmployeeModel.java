00ff8a1c1d69a623a6b4a973d40da6d83d9865d8
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-09 11:56:37 +0530

    ADDED : Add the employees as well as save the employee details in the database

---
+++ main/java/com/bridgelabz/employeepayrollapp/model/EmployeeModel.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Employee")
+@Data
+public class EmployeeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private long salary;
+    private String companyName;
+    private String department;
+    private LocalDateTime registerDate;
+    private LocalDateTime updatedDate;
+
+    public EmployeeModel(EmployeeDTO employeeDTO) {
+        this.firstName = employeeDTO.getFirstName();
+        this.lastName = employeeDTO.getLastName();
+        this.age = employeeDTO.getAge();
+        this.salary = employeeDTO.getAge();
+        this.companyName = employeeDTO.getCompanyName();
+        this.department = employeeDTO.getDepartment();
+    }
+
+    public EmployeeModel() {
+
+    }
+}
